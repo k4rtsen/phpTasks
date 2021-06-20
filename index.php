39436b38780ae40a9833f9f230d65b19968a3063
k4rtsen <karen.ayv12599@gmail.com>
2021-06-20 21:44:45 +0300

    let start

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        // this func finds prime numbers between args $start and $end
+        function FindSimple($start, $end)
+        {
+            $simpleNums = array();
+            if (abs($start) > abs($end))
+                echo true;
+
+        }
+        FindSimple("qwer", "fdas");
+    ?>
+</body>
+</html>
