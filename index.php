2d7cf868ed628f360e6b279af0ad95c06505e04a
k4rtsen <karen.ayv12599@gmail.com>
2021-06-21 23:12:05 +0300

    2nd task done

--- index.php
+++ index.php
@@ -10,11 +10,12 @@
 
 <body>
     <?php
+    // 1st TASK----------------------------------
     // this func finds prime numbers between args $start and $end
     function FindSimple($start, $end)
     {
         if (!is_int($start) || !is_int($end)) {
-            return false;
+            return NULL;
         }
 
         $simple = array();
@@ -29,10 +30,66 @@
         }
         return $simple;
     }
-    echo "<h3>1 task: </h3>";
+    echo "<h3>1st task: </h3>";
     echo "<h4>Result of function FindSimple: </h4>";
-    print_r(FindSimple(1, 12));
+    print_r(FindSimple(1, 15));
+    //-------------------------------------------
 
+    // 2nd TASK----------------------------------
+    function CreateTrapeze($a)
+    {
+        if ( !is_array($a) || (count($a) % 3 != 0))
+            return NULL;
+
+        $trapezeList = [];
+        $j = 0;
+        for ($i = 0; $i < count($a) / 3; $i++) {
+            $trapezeList[] = array(
+                'a' => $a[$j],
+                'b' => $a[$j + 1],
+                'c' => $a[$j + 2]
+            );
+            $j += 3;
+        }
+        return $trapezeList;
+    }
+    echo "<h3>2nd task: </h3>";
+    echo "<h4>Result of function CreateTrapeze: </h4>";
+    $arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    print_r(CreateTrapeze(1));
+    //-------------------------------------------
+
+    // 3rd TASK----------------------------------
+    function SquareTrapeze($a)
+    {
+    }
+
+    //-------------------------------------------
+
+    // 4th TASK----------------------------------
+    function getSizeForLimit($a, $b)
+    {
+    }
+
+    //-------------------------------------------
+
+    // 5th TASK----------------------------------
+    function getMin($a)
+    {
+    }
+
+    //-------------------------------------------
+
+    // 6th TASK----------------------------------
+    function printTrapeze($a)
+    {
+    }
+
+    //-------------------------------------------
+
+    // 7th TASK----------------------------------
+
+    //-------------------------------------------
     ?>
 </body>
 
