2a1a3c65730ec4a69db435f1212c54c5a494891b
kartsen <karen.ayv12599@gmail.com>
2021-07-13 18:54:12 +0300

    created function importXml

--- 2.php
+++ 2.php
@@ -12,7 +12,7 @@
     <?php
     // 1st task ---------------------------------
     // flips a substring in a string at the second position
-    function convertString(string $str, string $substr) : string
+    function convertString(string $str, string $substr): string
     {
         // if exist the second position
         if (substr_count($str, $substr) < 2)
@@ -31,7 +31,8 @@
     //-------------------------------------------
 
     // 2nd task ---------------------------------
-    function mySortForKey(array &$a, $b = null) {
+    function mySortForKey(array &$a, $b = null)
+    {
         // checks
         // you must set key $b
         if ($b == null) throw new Exception("you must set key for array");
@@ -49,18 +50,126 @@
         // return true;
     }
 
-    $someArr = [[   
-            'a' => 1,
-            'b' => 7
-        ],[ 'a' => 6,
-            'b' => 4
-        ],[ 'a' => 3,
-            'b' => 3
-        ]];
+    $someArr = [[
+        'a' => 1,
+        'b' => 7
+    ], [
+        'a' => 6,
+        'b' => 4
+    ], [
+        'a' => 3,
+        'b' => 3
+    ]];
     echo "<h3>2nd task: </h3>";
     echo "<h4>Result of function mySortForKey: </h4>";
     mySortForKey($someArr, 'b');
     print_r($someArr);
+    //-------------------------------------------
+
+    // Last task---------------------------------
+    function importXml($path)
+    {
+        //connecting to XML file
+        $xml = simplexml_load_file($path) or die("could not find file!");
+        //connecting to BD
+        $mysqli = mysqli_connect("localhost", "root", "", "test_samson");
+        /*
+        IMPORT TO BD
+        */
+        // fill the table a_category
+        foreach ($xml as $product) {
+            $sections = $product->sections->section;
+            foreach ($sections as $value) {
+                $query = "SELECT * FROM a_category";
+                $categories = mysqli_query($mysqli, $query);
+                $flag = true;
+                // checking for identical lines
+                foreach ($categories as $cat)
+                    if ($cat['name'] == $value) $flag = false;
+                if ($flag) {
+                    $query = "INSERT INTO a_category (name) VALUES ('$value')";
+                    $result = mysqli_query($mysqli, $query);
+                    echo $value;
+                } else continue;
+            }
+        }
+        // fill the table a_product
+        foreach ($xml as $product) {
+            $query = "SELECT * FROM a_category";
+            $categories = mysqli_query($mysqli, $query);
+            $section = $product->sections->section;
+            // checking for identical lines
+            $query = "SELECT * FROM a_product";
+            $products = mysqli_query($mysqli, $query);
+            $flag = true;
+            foreach ($products as $p)
+                if ($p['code'] == $product['code'] && $p['name'] == $product['name'])
+                    $flag = false;
+            // definition of fields
+            if ($flag) {
+                foreach ($section as $sec)
+                    foreach ($categories as $cat)
+                        if ($sec == $cat['name'])
+                            $id = $cat['ID'];
+                $code = $product['code'];
+                $name = $product['name'];
+                $query = "INSERT INTO a_product (code, product_type, name) VALUES ('$code', '$id', '$name')";
+                $result = mysqli_query($mysqli, $query);
+            } else continue;
+        }
+        // fill the table a_price
+        foreach ($xml as $product) {
+            $price = $product->price;
+            $query = "SELECT * FROM a_price";
+            $a_price = mysqli_query($mysqli, $query);
+            // checking for identical lines
+            foreach ($price as $pr) {
+                $flag = true;
+                foreach ($a_price as $a_pr)
+                    if (
+                        $product['code'] == $a_pr['product_code'] &&
+                        $pr['type'] == $a_pr['typePrice'] &&
+                        $pr == $a_pr['price']
+                    ) {
+                        $flag = false;
+                        break;
+                    }
+                if ($flag) {
+                    $product_code = $product['code'];
+                    $type_price = $pr['type'];
+                    $query = "INSERT INTO a_price (product_code, typePrice, price) VALUES ('$product_code', '$type_price', '$pr')";
+                    $result = mysqli_query($mysqli, $query);
+                } else continue;
+            }
+        }
+        // fill the table a_property
+        foreach ($xml as $product) {
+            $property = $product->property;
+            foreach ($property as $value) {
+                foreach ($value as $key => $val) {
+                    $query = "SELECT * FROM a_property";
+                    $a_property = mysqli_query($mysqli, $query);
+                    $flag = true;
+                    $product_code = $product['code'];
+                    $name_property = $key;
+                    $val_property = $val;
+                    foreach ($a_property as $a_pr)
+                        if (
+                            $a_pr['product_code'] == $product_code && $a_pr['name_property'] == $name_property && $a_pr['val_property'] == $val_property
+                        ) {
+                            $flag = false;
+                            break;
+                        }
+                    if ($flag) {
+                        $query = "INSERT INTO a_property (product_code, name_property, val_property) VALUES ('$product_code', '$name_property', '$val_property')";
+                        $result = mysqli_query($mysqli, $query);
+                    }
+                }
+            }
+        }
+    }
+    echo "<br>";
+    importXml("2.xml");
     //-------------------------------------------
     ?>
 </body>
